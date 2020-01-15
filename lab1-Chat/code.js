6ccea585ccbdb2051d48ae64eb29bd80c1d585c2
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-01-15 14:26:21 +0100

    Add js to append messages

---
+++ lab1-Chat/code.js
@@ -0,0 +1,44 @@
+////////////////////////////////////////////
+let writeMessageInput = document.getElementById('sendMessageInput');
+writeMessageInput.addEventListener('keypress', function(e) {
+  if(e.key === 'Enter') {
+    sendMessage();
+  }
+});
+
+////////////////////////////////////////////
+function sendMessage() {
+  // TODO: Use server
+  
+  let textMessage = getMessage();
+  appendMessage(textMessage);
+  clearInput();
+}
+
+////////////////////////////////////////////
+function getMessage() {
+  return document.getElementById('sendMessageInput').value;
+}
+
+function appendMessage(text) {
+  let messageListContainer = document.getElementById('container-messages');
+  let messageContainer = document.createElement('div');
+  let username = document.createElement('div');
+  let textMessageElement = document.createElement('p');
+  
+
+  username.innerHTML = 'Xavi';
+  textMessageElement.innerHTML = text;
+  
+  messageContainer.className = 'message-container';
+  username.className = 'message-username';
+  
+  messageListContainer.append(messageContainer);
+  messageContainer.append(username);
+  messageContainer.append(textMessageElement);
+}
+
+function clearInput() {
+
+}
+////////////////////////////////////////////
