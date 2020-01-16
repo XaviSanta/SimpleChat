40f627b0c916a5d69b2b026f389db7e5616a9241
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-01-16 17:12:10 +0100

    Add communication between users through server

--- lab1-Chat/code.js
+++ lab1-Chat/code.js
@@ -1,34 +1,47 @@
 // Store The username and room that the user inputs
+var server = new SillyClient();
 var username;
+var my_id;
 var roomNumber;
-
 ////////////////////////////////////////////
 
 function sendLogin() {
   username = document.getElementById('username-input').value; 
-  roomNumber = document.getElementById('room-input').value;
+  roomName = document.getElementById('room-input').value;
   
   // TODO: initiate connection with server
-  
-  // Enter room:
+  server.connect('wss://tamats.com:55000', roomName);
+  server.on_ready = function(id) {
+    my_id = id;
+    console.log('Hello, your id is: ' + my_id);
+    username = 'User_'+ my_id;
+  }
+
   // Change roomNumber
   let chatTitle = document.getElementById('chat-title')
-  chatTitle.innerHTML = 'Room ' + roomNumber;
+  chatTitle.innerHTML = roomName;
 }
 
 ////////////////////////////////////////////
 let writeMessageInput = document.getElementById('sendMessageInput');
 writeMessageInput.addEventListener('keypress', function(e) {
   if(e.key === 'Enter') {
-    sendMessage();
+    let msg = getMessage();
+    sendMessage(msg);
   }
 });
 
 ////////////////////////////////////////////
-function sendMessage() {
-  let textMessage = getMessage();
-  appendMessage(textMessage);
+function sendMessage(msg) {
+  server.sendMessage(msg);
+  appendMessage(my_id, msg);
   clearInput();
+}
+
+server.on_message = function(author_id, msg) {
+  console.log(author_id);
+  console.log(msg);
+  appendMessage(author_id, msg);
 }
 
 ////////////////////////////////////////////
@@ -36,16 +49,19 @@ function getMessage() {
   return document.getElementById('sendMessageInput').value;
 }
 
-function appendMessage(text) {
+function appendMessage(author_id, text) {
   let messageListContainer = document.getElementById('container-messages');
   let messageContainer = document.createElement('div');
   let usernameDiv = document.createElement('div');
   let textMessageP = document.createElement('p');
   
-  usernameDiv.innerHTML = username;
+  usernameDiv.innerHTML = author_id;
   textMessageP.innerHTML = text;
   
-  messageContainer.className = 'our message-container';
+  author_id == my_id ? 
+    messageContainer.className = 'our message-container' :
+    messageContainer.className = 'message-container';
+    
   usernameDiv.className = 'message-username';
   
   messageListContainer.append(messageContainer);
